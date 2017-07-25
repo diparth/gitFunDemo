dd7a21915b0f9ee7055c3bb04f34e3b42bd2b3ad
Diparth Patel <dpate168@nyit.edu>
2017-07-24 22:41:17 -0400

    Touch me button and activity added to ViewController MyLabel has been added.

--- gitFunDemo/ViewController.swift
+++ gitFunDemo/ViewController.swift
@@ -9,16 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello World!")
+        self.myLabel.text = ""
+        print("This is Git tutorial!")
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func btnPressed(_ sender: UIButton) {
+        self.myLabel.text = "Hello Nick!"
+        
     }
 
 
