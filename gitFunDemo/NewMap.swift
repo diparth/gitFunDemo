98f20956e2e7a90fc290691436debabb3ef7ab3a
Diparth Patel <dpate168@nyit.edu>
2017-07-24 23:39:11 -0400

    NewMap - Properties (name, and type) and Init added.

---
+++ gitFunDemo/NewMap.swift
@@ -0,0 +1,21 @@
+//
+//  NewMap.swift
+//  gitFunDemo
+//
+//  Created by Diparth Patel on 7/24/17.
+//  Copyright © 2017 Diparth Patel. All rights reserved.
+//
+
+import UIKit
+
+class NewMap: NSObject {
+    
+    var name: String?
+    var type: String?
+    
+    init(with name: String, type: String) {
+        self.name = name
+        self.type = type
+    }
+
+}
